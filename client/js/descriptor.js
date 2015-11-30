e878f44892ef2dc06ef8115d003e83da2331564e
Jeremy Privett <jeremy@jeremyprivett.com>
2015-11-30 08:01:47 +0000

    Initial commit.

---
+++ client/js/descriptor.js
@@ -0,0 +1,46 @@
+/**
+ * Descriptor - This class controls all interaction between the game and the client.
+ */
+
+function Descriptor() {
+    this.lastCommand = "";
+}
+
+Descriptor.prototype.handleInput = function(e) {
+    e.which = e.which || e.keyCode;
+    if (e.which == 13) {
+        this.handleOutput(e.target.value);
+        this.lastCommand = e.target.value || this.lastCommand; // Don't overwrite last command with empty string
+        e.target.value = "";
+    }
+    
+    // Support up arrow to get last entered command.
+    if (e.which == 38 && e.target.value == "") {
+        e.target.value = this.lastCommand;
+    }
+};
+
+Descriptor.prototype.handleOutput = function(text, additionalClasses) {
+    var div = document.createElement('div');
+    var screenContainer = document.getElementById('screen-container');
+    var mainScreen = document.getElementById('main-screen');
+    additionalClasses = additionalClasses || [];
+    
+    div.className = "console-line";
+    
+    if (additionalClasses instanceof Array && additionalClasses.length > 0) {
+        var classes = " " + additionalClasses.join(" ");
+        div.className += classes;
+    }
+    
+    div.innerHTML = text || "&nbsp;";
+    
+    mainScreen.appendChild(div);
+    screenContainer.scrollTop = screenContainer.scrollHeight;
+};
+
+Descriptor.prototype.handleClick = function() {
+    if (typeof window.getSelection == "undefined" || window.getSelection().toString() == "") {
+        document.getElementById("command-box").focus();
+    }
+};
---
+++ client/js/http.js
@@ -0,0 +1,10 @@
+// Very basic HTTP lib to fetch content
+function http(url, callback) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.onreadystatechange = function() {
+        if (r.readyState != 4 || r.status != 200) return;
+        callback(r.responseText);
+    };
+    r.send();
+}
